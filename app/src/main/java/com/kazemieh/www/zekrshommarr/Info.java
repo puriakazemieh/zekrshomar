e227561e6d059633dde191f0c02c6c49c98f8a07
puriakazemieh <puriakazemieh@gmail.com>
2020-12-04 10:52:41 +0330

    Modify codes

--- app/src/main/java/com/kazemieh/www/zekrshommarr/Info.java
+++ app/src/main/java/com/kazemieh/www/zekrshommarr/Info.java
@@ -1,6 +1,7 @@
 package com.kazemieh.www.zekrshommarr;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,7 @@ public class Info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        TextView textView=findViewById(R.id.tv_info);
+        textView.setText(R.string.infostring);
     }
 }
--- app/src/main/java/com/kazemieh/www/zekrshommarr/MainActivity.java
+++ app/src/main/java/com/kazemieh/www/zekrshommarr/MainActivity.java
@@ -2,12 +2,15 @@ package com.kazemieh.www.zekrshommarr;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -64,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 SharedPreferences.Editor editor=sharedPreferences.edit();
                 editor.putInt("number",c);
                 editor.apply();
+                Toast.makeText(MainActivity.this, "عدد ذخیره شد", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -83,6 +87,10 @@ public class MainActivity extends AppCompatActivity {
         }
         if (item.getItemId() == R.id.nazar) {
             // Toast.makeText(this, "ارتباط با ما کلیک شد", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Intent.ACTION_EDIT);
+            intent.setData(Uri.parse("bazaar://details?id=" + "com.kazemieh.www.zekrshommarr"));
+            intent.setPackage("com.farsitel.bazaar");
+            startActivity(intent);
 
         }
         return super.onOptionsItemSelected(item);
